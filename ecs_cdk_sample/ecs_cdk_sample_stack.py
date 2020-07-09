fb660d4fdcaa6e44f391cd8d3a4b30f5c20242c8
yohei1126 <yohei.onishi11@gmail.com>
2020-07-09 17:37:00 +0800

    init

--- ecs_cdk_sample/ecs_cdk_sample_stack.py
+++ ecs_cdk_sample/ecs_cdk_sample_stack.py
@@ -1,5 +1,5 @@
-from aws_cdk import core
-
+from aws_cdk import (core, aws_ec2 as ec2, aws_ecs as ecs,
+                     aws_ecs_patterns as ecs_patterns)
 
 class EcsCdkSampleStack(core.Stack):
 
@@ -7,3 +7,15 @@ class EcsCdkSampleStack(core.Stack):
         super().__init__(scope, id, **kwargs)
 
         # The code that defines your stack goes here
+        vpc = ec2.Vpc(self, "MyVpc", max_azs=3)     # default is all AZs in region
+
+        cluster = ecs.Cluster(self, "MyCluster", vpc=vpc)
+
+        ecs_patterns.ApplicationLoadBalancedFargateService(self, "MyFargateService",
+            cluster=cluster,            # Required
+            cpu=512,                    # Default is 256
+            desired_count=6,            # Default is 1
+            task_image_options=ecs_patterns.ApplicationLoadBalancedTaskImageOptions(
+                image=ecs.ContainerImage.from_registry("amazon/amazon-ecs-sample")),
+            memory_limit_mib=2048,      # Default is 512
+            public_load_balancer=True)  # Default is False
